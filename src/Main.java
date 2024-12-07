4ad46c3ec4dc1bce857520bdb2e8d3467bdcf6ad
Germano Macieira <germanomacieira@hotmail.com>
2024-12-06 21:06:55 -0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("****************************************************");
+        System.out.println("Seja bem-vindo/a ao Conversor de Moeda =]");
+            System.out.println("1) Dólar =>> Peso argentino");
+            System.out.println("2) Peso argentino =>> Dólar");
+            System.out.println("3) Dólar =>> Real brasileiro");
+            System.out.println("4) Real brasileiro =>> Dólar");
+            System.out.println("5) Dólar =>> Peso colombiano");
+            System.out.println("6) Peso colombiano =>> Dólar");
+            System.out.println("7) Sair");
+            System.out.println("****************************************************");
+            System.out.print("Escolha uma opção válida: ");
+        }
     }
 }
