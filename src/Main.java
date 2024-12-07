84b21fb83ff34b93dc96f1e887d9ce1d3fec3933
Germano Macieira <germanomacieira@hotmail.com>
2024-12-07 09:33:57 -0300

    Acrescentando o loop da tela inicial

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("****************************************************");
-        System.out.println("Seja bem-vindo/a ao Conversor de Moeda =]");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("****************************************************");
+            System.out.println("Seja bem-vindo/a ao Conversor de Moeda =]");
             System.out.println("1) Dólar =>> Peso argentino");
             System.out.println("2) Peso argentino =>> Dólar");
             System.out.println("3) Dólar =>> Real brasileiro");
@@ -13,5 +19,13 @@ public class Main {
             System.out.println("****************************************************");
             System.out.print("Escolha uma opção válida: ");
 
+            int opcao = scanner.nextInt();
+
+            if (opcao == 7) {
+                continuar = false;
+                System.out.println("Obrigado por usar o Conversor de Moeda. Até logo!");
+                break;
+            }
+        }
     }
 }
