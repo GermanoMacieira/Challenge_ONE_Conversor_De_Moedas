60c09bf4004ecab4932daaeb65d82109aa22c942
Germano Macieira <germanomacieira@hotmail.com>
2024-12-06 21:10:23 -0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("****************************************************");
@@ -15,6 +12,6 @@ public class Main {
             System.out.println("7) Sair");
             System.out.println("****************************************************");
             System.out.print("Escolha uma opção válida: ");
-        }
+
     }
 }
