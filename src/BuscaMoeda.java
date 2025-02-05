e4467983c3f877f57472ac6fe93918dadf82525f
Germano Macieira <69836135+GermanoMacieira@users.noreply.github.com>
2025-02-05 10:32:23 -0300

    Update BuscaMoeda

--- src/BuscaMoeda.java
+++ src/BuscaMoeda.java
@@ -6,7 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class BuscaMoeda {
-        private static final String API_KEY = "63f1a6019494d5d430b77a";
+        private static final String API_KEY = "Your_API_KEY";
         private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/";
 
         public static double obterTaxa(String moedaOrigem, String moedaDestino) {
